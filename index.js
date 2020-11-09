fd6cc50515aeddd9e7c52da7cb7253b21c81b4b3
apandiri21 <apandiri@usca.edu>
2020-11-09 13:13:31 -0500

    11/09 exercise

--- index.js
+++ index.js
@@ -17,7 +17,16 @@ import * as firebaseui from "firebaseui";
 
 // Add Firebase project configuration object here
 var firebaseConfig = {
-
+  const firebaseConfig = {
+  apiKey: "AIzaSyBxp6X1OSsbS_VRxIlZDXC3cLE1OffbKCE",
+  authDomain: "grocerylist-6f38e.firebaseapp.com",
+  databaseURL: "https://grocerylist-6f38e.firebaseio.com",
+  projectId: "grocerylist-6f38e",
+  storageBucket: "grocerylist-6f38e.appspot.com",
+  messagingSenderId: "389435855205",
+  appId: "1:389435855205:web:2667f96e169ad872cc918c",
+  measurementId: "G-MTSB5QWTT1"
+};
 };
 
 firebase.initializeApp(firebaseConfig);
